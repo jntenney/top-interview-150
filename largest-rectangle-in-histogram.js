b2af7953ec81b5a4b8a38aa325016b87e3d09f68
Jasen Tenney <jasen_tenney@hotmail.com>
2024-11-09 10:02:48 -0600

    Completed largest-rectangle-in-histogram

--- largest-rectangle-in-histogram.js
+++ largest-rectangle-in-histogram.js
@@ -16,11 +16,40 @@
  */
 function largestRectangleArea(heights) {
   let maxArea = 0;
-  let stack = [];
+  const stack = []; // holds [index, height] of an item
+  let index;
+
+  for (index = 0; index < heights.length; index++) {
+    let itemStart = index;
+
+    // while height of item on the stack is greater than current item then the item on stack has ended
+    // it also means the current item can extend back, so get the start index for the current item
+    while (stack.length && stack[stack.length - 1][1] > heights[index]) {
+      const topOfStack = stack.pop();
+      const stackItemArea = (index - topOfStack[0]) * topOfStack[1]; // length of rectangle * height
+      maxArea = Math.max(maxArea, stackItemArea);
+      itemStart = topOfStack[0];
+    }
+
+    stack.push([itemStart, heights[index]]);
+    stack;
+  }
+
+  // there may still be items on the stack, calculate area for each item
+  for (let i = 0; i < stack.length; i++) {
+    maxArea = Math.max(maxArea, (index - stack[i][0]) * stack[i][1]); // length of rectangle * height
+  }
 
   return maxArea;
 }
 
-let heights = [2, 1, 5, 6, 2, 3];
-let result = largestRectangleArea(heights);
-result;
+// let heights;
+// let result;
+
+// heights = [2, 1, 5, 6, 2, 3];
+// result = largestRectangleArea(heights);
+// result;
+
+// heights = [2, 2, 5, 4, 2, 3];
+// result = largestRectangleArea(heights);
+// result;
