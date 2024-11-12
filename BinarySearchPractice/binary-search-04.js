208f5b53438bc558d774c76dd27246e2c29f3472
Jasen Tenney <jasen_tenney@hotmail.com>
2024-11-12 08:39:20 -0600

    Completed find-minimum-rotated-sorted-array

--- BinarySearchPractice/binary-search-04.js
+++ BinarySearchPractice/binary-search-04.js
@@ -15,18 +15,39 @@
  * @param {number} target
  * @return {number}
  */
-function search(nums, target) {}
+function search(nums, target) {
+  // l = left side of range, inclusive
+  // r = right side of range inclusive
+  // mid = (l + r) / 2, floor
+  // while(l <= r)
+  let l = 0;
+  let r = nums.length - 1;
+
+  while (l <= r) {
+    const mid = Math.floor((l + r) / 2);
+
+    if (nums[mid] === target) return mid;
+
+    if (target > nums[mid]) {
+      l = mid + 1;
+    } else {
+      r = mid - 1;
+    }
+  }
+
+  return -1;
+}
 
 let nums;
 let target;
 let result;
 
 nums = [-1, 0, 2, 4, 6, 8];
-target = 8;
+target = 6;
 result = search(nums, target);
 result;
 
 nums = [-15, -9, -2, 0, 3, 16, 22];
-target = 0;
+target = 23;
 result = search(nums, target);
 result;
---
+++ find-minimum-rotated-sorted-array.js
@@ -0,0 +1,68 @@
+// You are given an array of length n which was originally sorted in ascending order. It has now been rotated between 1 and n times. For example, the array nums = [1,2,3,4,5,6] might become:
+// [3,4,5,6,1,2] if it was rotated 4 times.
+// [1,2,3,4,5,6] if it was rotated 6 times.
+// Notice that rotating the array 4 times moves the last four elements of the array to the beginning. Rotating the array 6 times produces the original array.
+// Assuming all elements in the rotated sorted array nums are unique, return the minimum element of this array.
+// A solution that runs in O(n) time is trivial, can you write an algorithm that runs in O(log n) time?
+// Example 1:
+// Input: nums = [3,4,5,6,1,2]
+// Output: 1
+// Example 2:
+// Input: nums = [4,5,0,1,2,3]
+// Output: 0
+// Example 3:
+// Input: nums = [4,5,6,7]
+// Output: 4
+// Constraints:
+// 1 <= nums.length <= 1000
+// -1000 <= nums[i] <= 1000
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+function findMin(nums) {
+  // l = left side of range, inclusive
+  // r = right side of range, inclusive
+  // mid = (l+r) / 2, floor
+  // while(l <= r)
+
+  let l = 0;
+  let r = nums.length - 1;
+  let result = nums[0];
+
+  while (l <= r) {
+    if (nums[l] < nums[r]) {
+      result = Math.min(result, nums[l]);
+      break;
+    }
+
+    const mid = Math.floor((l + r) / 2);
+    result = Math.min(result, nums[mid]);
+
+    if (nums[mid] >= nums[l]) {
+      l = mid + 1;
+    } else {
+      r = mid - 1;
+    }
+  }
+
+  return result;
+}
+
+let nums;
+let result;
+nums = [3, 4, 5, 6, 1, 2];
+result = findMin(nums);
+result;
+
+nums = [5, 1, 2, 3, 4];
+result = findMin(nums);
+result;
+
+nums = [4, 5, 0, 1, 2, 3];
+result = findMin(nums);
+result;
+
+nums = [10, 20, 30, 40, 50, 60, 70];
+result = findMin(nums);
+result;
