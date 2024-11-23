86f4e4f3970f303dd6044418014df36cc467d05d
Jasen Tenney <jasen_tenney@hotmail.com>
2024-11-23 12:14:10 -0600

    completed remove-node-from-end-of-linked-list

---
+++ remove-node-from-end-of-linked-list.js
@@ -0,0 +1,90 @@
+// You are given the beginning of a linked list head, and an integer n.
+// Remove the nth node from the end of the list and return the beginning of the list.
+// Example 1:
+// Input: head = [1,2,3,4], n = 2
+// Output: [1,2,4]
+// Example 2:
+// Input: head = [5], n = 1
+// Output: []
+// Example 3:
+// Input: head = [1,2], n = 2
+// Output: [2]
+// Constraints:
+// The number of nodes in the list is sz.
+// 1 <= sz <= 30
+// 0 <= Node.val <= 100
+// 1 <= n <= sz
+class ListNode {
+  constructor(val = 0, next = null) {
+    this.val = val;
+    this.next = next;
+  }
+}
+
+/**
+ * @param {number[]} nums
+ * @return {ListNode}
+ */
+function createLinkedList(nums) {
+  let prev = null;
+  let head = null;
+
+  for (let i = nums.length - 1; i >= 0; i--) {
+    head = new ListNode(nums[i], prev);
+    prev = head;
+  }
+
+  return head;
+}
+
+/**
+ * @param {ListNode} head
+ * @param {number} n
+ * @return {ListNode}
+ */
+function removeNthFromEnd(head, n) {
+  if (!head) return head;
+  if (n <= 0) return head;
+
+  let newHead = new ListNode();
+  newHead.next = head;
+
+  let fNode = newHead;
+  let bNode = newHead;
+
+  let count = 0;
+  for (count = 0; count < n + 1 && fNode; count++) {
+    fNode = fNode.next;
+  }
+
+  // if there are less then n nodes in the list then just return the head without removing nth node
+  if (count - 1 !== n) return head;
+
+  while (fNode) {
+    fNode = fNode.next;
+    bNode = bNode.next;
+  }
+
+  bNode.next = bNode.next.next;
+
+  return newHead.next;
+}
+
+let ll, head, n, result;
+head = [1, 2, 3, 4];
+n = 2;
+ll = createLinkedList(head);
+result = removeNthFromEnd(ll, n);
+result;
+
+head = [5];
+n = 1;
+ll = createLinkedList(head);
+result = removeNthFromEnd(ll, n);
+result;
+
+head = [1, 3, 5];
+n = 4;
+ll = createLinkedList(head);
+result = removeNthFromEnd(ll, n);
+result;
